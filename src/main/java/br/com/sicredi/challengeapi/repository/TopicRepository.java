8549835e5199ffa0d225810eebbec074591511bd
Ian Farias Paixão <ianfariaspaixao14@gmail.com>
2023-01-23 11:28:52 -0300

    list all topics

--- src/main/java/br/com/sicredi/challengeapi/repository/TopicRepository.java
+++ src/main/java/br/com/sicredi/challengeapi/repository/TopicRepository.java
@@ -3,7 +3,9 @@ package br.com.sicredi.challengeapi.repository;
 import br.com.sicredi.challengeapi.model.Topic;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
+import java.util.ArrayList;
 
-public interface TopicRepository extends CrudRepository<Topic, UUID> {
+
+public interface TopicRepository extends CrudRepository<Topic, Long> {
+    ArrayList<Topic> findAll();
 }
